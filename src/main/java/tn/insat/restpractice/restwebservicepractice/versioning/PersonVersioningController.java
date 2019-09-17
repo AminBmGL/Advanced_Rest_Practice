e7dc4675881d84f6b81b526f21d022096369197d
Amin Bm <medaminbenmbarek@gmail.com>
2019-09-17 17:33:00 +0100

    Versioning RESTful Services - Header and Content Negotiation Approach

--- src/main/java/tn/insat/restpractice/restwebservicepractice/versioning/PersonVersioningController.java
+++ src/main/java/tn/insat/restpractice/restwebservicepractice/versioning/PersonVersioningController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
+	
+	//URI versioning 
 	@GetMapping("v1/person")
 	public PersonV1 personV1() {
 		return new PersonV1("Bob Charlie");
@@ -15,5 +17,49 @@ public class PersonVersioningController {
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
+	
+	//request Parameter versioning
+	
+	// /person/param?version=1
+	@GetMapping(value = "/person/param", params = "version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	// /person/param?version=2
+	@GetMapping(value = "/person/param", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
+	
+	//Header versioning
+	
+	// /person/header with an X-API-VERSION header equal to 1
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	// /person/header with an X-API-VERSION header equal to 2
+	@GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+	
+	//Accept header (called also Media type) versioning
+
+	// /person/procuces with Accept Header eqal to application/vnd.company.app-v1+json
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	// /person/procuces with Accept Header eqal to application/vnd.company.app-v2+json
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
 
 }
