cbd80d68fef7f6a9164e14b17efeaafa27ec0292
Amin Bm <medaminbenmbarek@gmail.com>
2019-09-18 00:49:03 +0100

    Clean some sql fault in userId

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@ insert into user values(1001, sysdate(), 'Amin');
 insert into user values(1002, sysdate(), 'Wissem');
 insert into user values(1003, sysdate(), 'Salah');
 
-insert into post values(11001, 'Have A nice day', 10001);
-insert into post values(11002, 'Learn Spring', 10001);
+insert into post values(11001, 'Have A nice day', 1001);
+insert into post values(11002, 'Learn Spring', 1001);
